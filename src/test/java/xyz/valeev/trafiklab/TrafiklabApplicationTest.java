0aa55623af8ae36412ef50bf2b9c46380aeaf79a
nail <otvet99@gmail.com>
2021-03-27 22:56:45 +0100

    Added Spring Boot dependencies

---
+++ src/test/java/xyz/valeev/trafiklab/TrafiklabApplicationTest.java
@@ -0,0 +1,11 @@
+package xyz.valeev.trafiklab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TrafiklabApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}

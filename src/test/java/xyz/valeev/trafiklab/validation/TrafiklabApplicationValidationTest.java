5918e088f74b336ad7a5f0d1065229fe3279dc2b
nail <otvet99@gmail.com>
2021-03-28 22:40:29 +0200

    Added validation test

--- src/test/java/xyz/valeev/trafiklab/validation/TrafiklabApplicationValidationTest.java
+++ src/test/java/xyz/valeev/trafiklab/validation/TrafiklabApplicationValidationTest.java
@@ -1,58 +1,46 @@
-package xyz.valeev.trafiklab.health;
+package xyz.valeev.trafiklab.validation;
 
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.web.bind.annotation.GetMapping;
 import xyz.valeev.trafiklab.controller.BusLinesController;
 import xyz.valeev.trafiklab.service.BusLinesService;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @WebMvcTest(value = BusLinesController.class)
 @Import({BusLinesController.class, BusLinesService.class})
-public class TrafiklabApplicationHealthTest {
+public class TrafiklabApplicationValidationTest {
 
-    @Value("${pom.version: not set}")
-    private String expectedPomVersion;
+    private final int INVALID_LINE_NUMBER = -2;
 
     @Autowired
     private MockMvc mockMvc;
 
-    RequestBuilder requestBuilder = MockMvcRequestBuilders.get(
-            "/api/bus/health").accept(
-            MediaType.APPLICATION_JSON);
+    RequestBuilder requestBuilder = MockMvcRequestBuilders
+            .get("/api/bus/v1/lines/" + INVALID_LINE_NUMBER)
+            .accept(MediaType.APPLICATION_JSON);
 
     @Test
-    public void health_check_should_be_successful_and_return_json() throws Exception {
+    public void when_line_number_is_invalid_then_return_bad_request() throws Exception {
         mockMvc.perform(requestBuilder)
-                .andExpect(status().is2xxSuccessful())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
-    }
-
-    @Test
-    public void health_check_should_return_version_number() throws Exception {
-        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-        System.out.println(result.getResponse());
-
-        JSONAssert.assertEquals(
-                "{Version: " + expectedPomVersion + "}",
-                result.getResponse().getContentAsString(),
-                true);
+                .andExpect(status().isBadRequest());
     }
 }
-
 
